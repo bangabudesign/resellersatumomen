33bba6c1987f6b40a6e6df516dbaa9584345b489
Abu Bakar Halid <71490052+bangabudesign@users.noreply.github.com>
2022-05-27 23:40:58 +0800

    Update Meta Heading & Description

--- invitation.php
+++ invitation.php
@@ -24,21 +24,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Undangan Digital</title>
-    <meta name="title" content="Undangan Digital">
-    <meta name="description" content="Undangan pernikahan online atau undangan pernikahan digital dengan desain website ekslusif.">
+    <meta name="title" content="<?php echo $invitation ? strip_tags($invitation->heading) : 'Undangan Digital' ?>">
+    <meta name="description" content="<?php echo $invitation ? strip_tags($invitation->introduction) : 'Undangan pernikahan online atau undangan pernikahan digital dengan desain website ekslusif.' ?>">
     <meta itemprop="image" content="<?php echo $invitation ? $invitation->cover_url : 'https://websiteanda.com/undangan/logo.jpg' ?>">
     
     <!-- Open Graph / Facebook -->
     <meta property="og:type" content="website">
     <meta property="og:url" content="https://websiteanda.com">
-    <meta property="og:title" content="Undangan Digital">
-    <meta property="og:description" content="Undangan pernikahan online atau undangan pernikahan digital dengan desain website ekslusif.">
+    <meta property="og:title" content="<?php echo $invitation ? strip_tags($invitation->heading) : 'Undangan Digital' ?>">
+    <meta property="og:description" content="<?php echo $invitation ? strip_tags($invitation->introduction) : 'Undangan pernikahan online atau undangan pernikahan digital dengan desain website ekslusif.' ?>">
     <meta property="og:image" content="<?php echo $invitation ? $invitation->cover_url : 'https://websiteanda.com/undangan/logo.jpg' ?>">
     
     <!-- Twitter Meta Tags -->
     <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:title" content="Undangan Digital">
-    <meta name="twitter:description" content="Undangan pernikahan online atau undangan pernikahan digital dengan desain website ekslusif.">
+    <meta name="twitter:title" content="<?php echo $invitation ? strip_tags($invitation->heading) : 'Undangan Digital' ?>">
+    <meta name="twitter:description" content="<?php echo $invitation ? strip_tags($invitation->introduction) : 'Undangan pernikahan online atau undangan pernikahan digital dengan desain website ekslusif.' ?>">
     <meta name="twitter:image" content="<?php echo $invitation ? $invitation->cover_url : 'https://websiteanda.com/undangan/logo.jpg' ?>">
 </head>
 
