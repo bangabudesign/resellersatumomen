7ed646a8287017a36db42fa05c93cddcbf945abc
Abu Bakar Halid <71490052+bangabudesign@users.noreply.github.com>
2023-09-21 13:23:05 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,11 @@
 <?php
 
 $request = $_SERVER['REQUEST_URI'];
-$path = '/inv/';
+$path = '/';
 
 switch ($request) {
     case $path :
         require __DIR__ . '/home.php';
-        break;
-    case $path."katalog" :
-        require __DIR__ . '/katalog.php';
-        break;
-    case $path."katalog/" :
-        require __DIR__ . '/katalog.php';
         break;
     default:
         require __DIR__ . '/invitation.php';
