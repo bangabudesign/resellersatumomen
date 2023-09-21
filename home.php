37f076e2ca5474b87359fa1388638ed9e9209d13
Abu Bakar Halid <71490052+bangabudesign@users.noreply.github.com>
2023-09-21 12:54:04 +0800

    Update home.php

--- home.php
+++ home.php
@@ -9,30 +9,24 @@
     <title>Undangan Digital</title>
     <meta name="title" content="Undangan Digital">
     <meta name="description" content="Undangan pernikahan online atau undangan pernikahan digital dengan desain website ekslusif.">
-    <meta itemprop="image" content="https://websiteanda.com/inv/logo.jpg">
+    <meta itemprop="image" content="https://websiteanda.com/inv/logo.jpg"> <!-- EDIT BAGIAN INI -->
     
     <!-- Open Graph / Facebook -->
     <meta property="og:type" content="website">
-    <meta property="og:url" content="https://websiteanda.com">
+    <meta property="og:url" content="https://websiteanda.com"> <!-- EDIT BAGIAN INI -->
     <meta property="og:title" content="Undangan Digital">
     <meta property="og:description" content="Undangan pernikahan online atau undangan pernikahan digital dengan desain website ekslusif.">
-    <meta property="og:image" content="https://websiteanda.com/inv/logo.jpg">
+    <meta property="og:image" content="https://websiteanda.com/inv/logo.jpg"> <!-- EDIT BAGIAN INI -->
     
     <!-- Twitter Meta Tags -->
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:title" content="Undangan Digital">
     <meta name="twitter:description" content="Undangan pernikahan online atau undangan pernikahan digital dengan desain website ekslusif.">
-    <meta name="twitter:image" content="https://websiteanda.com/inv/logo.jpg">
+    <meta name="twitter:image" content="https://websiteanda.com/inv/logo.jpg"> <!-- EDIT BAGIAN INI -->
 </head>
 
 <body style="position:fixed; top:0; right: 0; bottom:0; left:0; margin:0" class="bg-white">
-    <div class="flex flex-col container max-w-md mx-auto py-8 px-4">
-        <img class="rounded-full w-24 mx-auto mb-7 border-2 border-gray-100" src="/inv/logo.jpg" />
-        <div class="text-center text-xl font-bold mb-10">MYRASY</div>
-        <a href="/inv/katalog" class="py-4 px-3 bg-sky-800 hover:bg-sky-900  text-white text-center rounded-lg mb-4">Lihat Katalog Undangan</a>
-        <a href="https://wa.me/6285559350885?text=Halo+Saya+Mau+Pesan+Undangan+Digital" target="_BLANK" class="py-4 px-3 bg-sky-800 hover:bg-sky-900  text-white text-center rounded-lg mb-4">Hubungi Admin</a>
-        <a href="https://instagram.com/myrasy.co" target="_BLANK" class="py-4 px-3 bg-sky-800 hover:bg-sky-900  text-white text-center rounded-lg mb-4">@myrasy.co</a>
-    </div>
+<!-- EDIT BAGIAN INI -->
 </body>
 
 </html>
